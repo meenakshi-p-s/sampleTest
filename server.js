3a8bdeada532325f6da0328eb9710d8517250e72
meenakshi-p-s <psmeenakshi33@gmail.com>
2025-09-16 17:43:46 +0530

    recent commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end('node \n');
+  res.end('Server created successfully\n');
 });
 
 server.listen(3000, () => {
